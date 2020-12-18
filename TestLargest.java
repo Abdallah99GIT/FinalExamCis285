45ecc85fe819044eeee57f04e043dfba5a6284ea
Abdallah <Aalallaf@umich.edu>
2020-12-18 16:15:29 -0500

    Intial code before fix

---
+++ TestLargest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestLargest  {
+	private Largest temp1;
+
+	public void testPositive(){
+        int[] arr = new int[4];
+        arr[0] = 11;
+        arr[1] = 9;
+        arr[2] = 7;
+        arr[3] = 10;
+        temp1 = new Largest();
+        int maxValue = temp1.largest(arr);
+	/** add tests to check for this unit test **/
+    }
+
+public void testNegative(){
+        /** add tests to check for this unit test **/ }
